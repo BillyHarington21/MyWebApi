da482126e561a1dabf46cf10bc1e5ec85ee52b28
CENTURION 1 <kdtkachenko@gmail.com>
2024-10-19 17:30:15 +0300

    реализовал DTO для сущностей и интефейс с классом реализующим его, для работы с продуктами и их категориями

---
+++ Services/Interface/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using Domen.Entities;
+using Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interface
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product> GetProductByIdAsync(int id);
+        Task<Product> CreateProductAsync(ProductDTO productDto);
+        Task<bool> UpdateProductAsync(int id, ProductDTO productDto);
+        Task<bool> DeleteProductAsync(int id);
+    }
+}
